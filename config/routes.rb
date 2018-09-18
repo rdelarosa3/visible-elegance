9429dcaeef84e5437ff9cd3835e6d43a88e81fb2
rdelarosa3 <rdelarosa39@gmail.com>
2018-09-18 18:28:48 +0800

    user login created

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,29 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   	# Add a root route if you don't have one...
-	# We can use users#new for now, or replace this with the controller and action you want to be the site root:
+	####
 	root to: 'users#new'
   	
+  	#### CREATE USER ################
   	# sign up page with form:
 	get 'users/new' => 'users#new', as: :new_user
 	
 	# create (post) action for when sign up form is submitted:
 	post 'users' => 'users#create'
+
+	#### !CREATE USER ################
+
+	#### USER LOGIN ##################
+
+	# log in page with form:
+	get '/login'     => 'sessions#new'
+	
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+	
+	# delete action to log out:
+	delete '/logout' => 'sessions#destroy'
+
+	#### !USER LOGIN ##################
 end
