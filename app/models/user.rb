5177f1ae98e7001c4d8c1066f387ba77f7c243b3
rdelarosa3 <rdelarosa39@gmail.com>
2018-09-18 18:15:59 +0800

    created user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	has_secure_password
+
+	# Verifys that email field is not blank and that it doesn't already exist in the db (prevents duplicates)#
+	validates :email, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  	# Add a root route if you don't have one...
+	# We can use users#new for now, or replace this with the controller and action you want to be the site root:
+	root to: 'users#new'
+  	
+  	# sign up page with form:
+	get 'users/new' => 'users#new', as: :new_user
+	
+	# create (post) action for when sign up form is submitted:
+	post 'users' => 'users#create'
 end
