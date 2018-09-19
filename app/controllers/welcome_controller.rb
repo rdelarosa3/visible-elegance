70cb4bb40657e2170fb402f7612747d2a716ed99
rdelarosa3 <rdelarosa39@gmail.com>
2018-09-19 14:33:01 +0800

    @services added to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 	def homepage
 	  	@staff = User.all.where(role: :admin)
-	  	# @service_types = ServiceType.all.order('name ASC')
+	  	@service_types = ServiceType.all.order('name ASC')
   	end
 end
