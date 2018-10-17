7adfa4d583d37b605a675bfcbaab52aa147bc823
rdelarosa3 <rdelarosa39@gmail.com>
2018-10-17 18:52:33 +0800

    added book now button

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
 	def homepage
 	  	@staff = User.all.where(role: :admin)
 	  	@service_types = ServiceType.all.order('name ASC')
+	  	@reservation = Reservation.new
   	end
 end
