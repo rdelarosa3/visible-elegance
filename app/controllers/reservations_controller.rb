2d41b47ace6ac8fbe5e5cf48c3d80dc70101d401
rdelarosa3 <rdelarosa39@gmail.com>
2018-11-01 04:11:05 +0700

    configured reservations authorizations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :edit, :update, :destroy]
-  before_action :authorize, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, only: [:show, :edit, :update, :destroy, :schedule, :index]
   include UsersHelper
 
   def index
